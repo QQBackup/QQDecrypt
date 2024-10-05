59d7e0586b6052caebe4d84072faafa71d3ec517
aaqwq <172821606+weasUXL@users.noreply.github.com>
2024-10-06 00:46:07 +0800

    添加vercel/analytics

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,14 +1,22 @@
-// .vitepress/theme/index.js
-
+// .vitepress/theme/index.ts
 import { h } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import HashCalculator from './components/HashCalculator.vue'
+import { inject as vercelInject } from "@vercel/analytics" // 使用通用包
 import './style.css'
+import { Theme } from 'vitepress' // 确保导入 Theme 类型
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
     // 注册全局组件
     app.component('HashCalculator', HashCalculator)
+
+    // 仅在生产环境中注入 Vercel Analytics
+    if (process.env.NODE_ENV === 'production') {
+      vercelInject({
+        id: 'prj_87NykKrzbGHfoLWeerHSJv031hsJ'
+      })
+    }
   }
-}
+} satisfies Theme
